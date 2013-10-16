f57c7b9a0549df99e9a7511c0e8d2199e76c900a
Angel J Alvarez Miguel <angel@uah.es>
2013-10-16 09:51:43 +0200

    Fast bytesString parser
    This hirrible mess is a proof of concept that I still to learn how to
    use ByteStrings…
    
    Just a mess asking for a good death

---
+++ src/FastParser.hs
@@ -0,0 +1,157 @@
+
+
+module FastParser 
+    (
+        processFastFile
+        ) where
+
+import Data.Word
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.Attoparsec.ByteString.Lazy as ABL
+-- import Text.ParserCombinators.Parsec
+-- import ParsecNumbers
+
+
+data Element =
+      RData Double
+    | IData Int
+    | SData B.ByteString
+    | CData Char
+    | Space Int
+    
+data FileLine = FileLine Int String [Element]
+
+-- type StringParser st  = GenParser Char st String
+-- type LineParser st    = GenParser Char st FileLine
+-- type ElementParser st = GenParser Char st Element
+type ElementParser = ABL.Result FileLine
+type ParseError    = String
+
+
+processFastFile :: FilePath -> IO ()
+processFastFile file = do
+        putStrLn $ show file
+        result <- parseFile file
+        case result of
+             Left err  -> print err
+             Right xs  -> mapM_ dumpLine xs
+
+
+
+parseFile :: String -> IO ( Either ParseError [FileLine])
+parseFile fname = do
+    input <- BL.readFile fname
+    return $ ABL.eitherResult $ ABL.parse parseFastFile input
+
+parseFastFile :: ABL.Parser [FileLine]
+parseFastFile = do
+    elements <- ABL.many1 parseFileLine
+    ABL.endOfInput
+    return $ elements
+
+parseFileLine :: ABL.Parser FileLine
+parseFileLine = do
+--         parseNewLine
+--     <|> parseBlankLine
+--     <|> parseLine
+    parseAnyLine
+--     <|> parseAnyRest
+    ABL.<?> "kaka"
+
+newLine :: Word8 -> Bool
+newLine w = w == 13 || w == 10
+
+parseAnyLine :: ABL.Parser FileLine
+parseAnyLine = ABL.try $ do
+    elements <- ABL.takeTill newLine
+    return $ FileLine 10 "anyLine" [SData elements]
+
+dumpLine :: FileLine -> IO ()
+dumpLine l@(FileLine i d e) = do
+    putStrLn $ (show i) ++ " {" ++ d ++ "} " ++ linetrace ++ "     " ++ linecontents
+    where
+        linetrace = concat $ map traceElement e
+        linecontents = concat $ map dumpElement e
+        traceElement:: Element -> String
+        traceElement (RData _x) = "R"
+        traceElement (IData _x) = "I"
+        traceElement (SData _x) = "S"
+        traceElement (CData _x) = "C"
+        traceElement (Space 0)   = "*"
+        traceElement (Space 1)   = "_"
+        traceElement (Space x)   = "_" ++ show(x) ++ "_"
+        dumpElement:: Element -> String
+        dumpElement (RData x) = show x
+        dumpElement (IData x) = show x
+        dumpElement (SData x) = show x
+        dumpElement (CData x) = [x]
+        dumpElement (Space 0) = ""
+        dumpElement (Space 1) = " "
+        dumpElement (Space x) = Prelude.take x $ repeat ' '
+
+-- parseNewLine :: LineParser st
+-- parseNewLine = try $ do
+--     pos <- getPosition
+--     newline
+--     return $ FileLine (sourceLine pos) "Newline" [Space 0]
+-- 
+-- parseBlankLine :: LineParser st
+-- parseBlankLine = try $ do
+--     pos <- getPosition
+--     elements <- space `manyTill` newline
+--     return $ FileLine (sourceLine pos) "BlankLine" [Space (length elements)]
+-- 
+-- parseLine :: LineParser st
+-- parseLine = try $ do
+--     pos <- getPosition
+--     elements <- parseElement `manyTill` newline
+--     return $ FileLine (sourceLine pos) "Line" elements
+-- 
+-- 
+-- parseAnyRest :: LineParser st
+-- parseAnyRest = try $ do
+--     pos <- getPosition
+--     elements <- many1 anyChar
+--     return $ FileLine (sourceLine pos) "anyRest" [SData elements]
+-- 
+-- parseElement :: ElementParser st
+-- parseElement = do
+--     parseSpace
+--     <|> parseNum
+--     <|> parseWord
+-- 
+-- parseSpace :: ElementParser st
+-- parseSpace = do
+--     content <- many1 (char ' ')
+--     return $ Space (length content)
+-- 
+-- parseNum :: ElementParser st
+-- parseNum = do
+--         parseReal
+--     <|> parseNat
+-- 
+-- parseNat :: ElementParser st
+-- parseNat = try $ do
+--     number <- intNumber
+--     return $ IData number
+-- 
+-- parseReal :: ElementParser st
+-- parseReal = try $ do
+--     number <- realNumber
+--     return $ RData number
+-- 
+-- -- Parse a single char or a string of printable caracters
+-- parseWord :: ElementParser st
+-- parseWord = try $ do
+--     content <- many1 (noneOf " \n")
+--     return $ charOrString content
+--     where
+--         charOrString x  
+--             | (length x > 1) = SData x          -- Many matches so we return a string
+--             | otherwise      = CData (head x)   -- Single match so we return a char
+--         
+-- -- parseChar :: ElementParser st
+-- -- parseChar = try $ do
+-- --     c <- anyChar
+-- --     return $ CData c
